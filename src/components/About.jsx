af5f964cc8d14547bd48d27b6226c36e156c6fdf
Kirill Mezentsev <kirmez02@gmail.com>
2024-08-28 18:59:45 +0200

    fixed errors in camera

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+
+import { Tilt } from "react-tilt";
+
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
 
-export default About
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn("", "", 0.1, 1)}>
+        I'm a skilled web developer with experience in JavaScript and PHP, and
+        expertise in frameworks like React and Three.js. I'm a quick learner and
+        collaborate closely with clients to create efficient, scalable, and
+        user-friendly solutions that solve real-world problems. Let's work
+        together to bring your ideas to life
+      </motion.p>
+    </>
+  );
+};
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,10 +20,28 @@ const Hero = () => {
           </h1>
 
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop modern website, user <br className="sm:block hidden" />{" "}
+            I develop modern websites, user <br className="sm:block hidden" />{" "}
             interfaces and web applications
           </p>
         </div>
+      </div>
+
+      <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full mb-1 bg-secondary"
+            />
+          </div>
+        </a>
       </div>
     </section>
   );
